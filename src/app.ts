9d6e2113cabfa1c37417f8b5eb5f95c575dab48a
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-28 02:34:42 -0300

    refactor: improves error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+app.setErrorHandler((err, _, res) => {
+	if (err instanceof ZodError) {
+		return res
+			.status(400)
+			.send({ message: 'Validation error.', issues: err.format() })
+	}
+
+	if (env.NODE_ENV !== 'production') {
+		console.error(err)
+	} else {
+		// TODO: here we should log to an external tool for observability (like Datadog/NewRelic/Sentry)
+	}
+
+	return res.status(500).send({ message: 'Internal server error.' })
+})
--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -23,7 +23,7 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 			return res.status(409).send({ message: err.message })
 		}
 
-		return res.status(500).send() // TODO: fix me
+		throw err
 	}
 
 	return res.status(201).send()
