bd0df57914e30ea395d8a9f4502ab35ab9c42ac0
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-28 00:54:41 -0300

    chore: hashes user password

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 
@@ -11,11 +12,23 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 
 	const { name, email, password } = registerBodySchema.parse(req.body)
 
+	const userWithSameEmail = await prisma.user.findUnique({
+		where: {
+			email,
+		},
+	})
+
+	if (userWithSameEmail) {
+		return res.status(409).send() // Conflict
+	}
+
+	const password_hash = await hash(password, 6)
+
 	await prisma.user.create({
 		data: {
 			name,
 			email,
-			password_hash: password,
+			password_hash,
 		},
 	})
 
