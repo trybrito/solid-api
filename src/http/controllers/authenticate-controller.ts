fcd773ced040c8d4b9d7db89c4000274a01b1a9d
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-03-29 22:02:09 -0300

    refactor: introducing factory pattern to instance services with all dependencies

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateService } from '@/services/authenticate'
 import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
+import { makeAuthenticateService } from '@/services/factories/make-authenticate-service'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 
@@ -13,8 +12,7 @@ export async function authenticate(req: FastifyRequest, res: FastifyReply) {
 	const { email, password } = authenticateBodySchema.parse(req.body)
 
 	try {
-		const usersRepository = new PrismaUsersRepository()
-		const authenticateService = new AuthenticateService(usersRepository)
+		const authenticateService = makeAuthenticateService()
 
 		await authenticateService.execute({ email, password })
 	} catch (err) {
