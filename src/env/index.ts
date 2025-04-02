a2de54ff0e9161beef3399ee84603bcd165ae63a
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-02 01:13:51 -0300

    fix: removes BROWSER env variable (which is used only for stoping prisma studio from opening a new tab)

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,6 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-	BROWSER: z.string(),
 	NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
 	PORT: z.coerce.number().default(3333),
 	DATABASE_URL: z.string().url(),
