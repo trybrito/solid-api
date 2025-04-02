03b52a6158238674b6d4ae0209e84f228fe43db0
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-04-01 21:53:25 -0300

    feat: refresh token | test(e2e): refresh controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from './register-controller'
 import { authenticate } from './authenticate-controller'
 import { profile } from './profile-controller'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh-controller'
 
 export async function usersRoutes(app: FastifyInstance) {
 	app.post('/users', register)
 	app.post('/sessions', authenticate) // "Creating" a user session
+
+	app.patch('/token/refresh', refresh)
 
 	/* Access when authenticated */
 	app.get('/me', { onRequest: [verifyJwt] }, profile)
